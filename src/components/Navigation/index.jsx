5dec95c87ab94f2066c8ccb4837e82b3dbd7a276
Charlotte Valset <charlottevalset@gmail.com>
2024-05-07 12:53:49 +0200

    adds nav with cart icon w/overlay

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,60 @@
+
+
+export const Navigation = () => {
+
+  const cartItemCount = 5;
+
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl text-sky-900 font-semibold whitespace-nowrap dark:text-white">Vivace</span>
+        </a>
+        <ul className="flex gap-4">
+          <li>
+          <a className="nav-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+          <a className="nav-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+          <a className="relative nav-icon cart-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z"
+                />
+              </svg>
+              <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1.5 py-0.5">
+                {cartItemCount}
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};

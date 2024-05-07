b3f8db79e1f3b570733cc1d28d24be7876ef1d9e
Charlotte Valset <charlottevalset@gmail.com>
2024-05-07 09:44:55 +0200

    adds header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import './App.css'
+import { Layout } from './components/Layout'
 
 function App() {
 
   return (
     <>
       <div>
-<h1>Vivace </h1>
-</div>
+        <h1 className="text-3xl text-sky-800">
+        Vivace</h1>
+        <Layout />
+      </div>
     </>
   )
 }

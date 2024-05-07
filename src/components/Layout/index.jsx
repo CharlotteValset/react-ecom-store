5b52eabda006c95fa38695442cf87d4f811289f8
Charlotte Valset <charlottevalset@gmail.com>
2024-05-07 09:44:21 +0200

    adds Layout component

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./Header"
+import { Footer } from "./Footer"
+import { ProductCard } from "../Card"
+
+
+export const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <ProductCard />
+      <Footer />
+    </div>
+  )
+}

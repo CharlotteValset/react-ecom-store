04944ba4ec1618e54dc1394be76bac3f9a395e2f
Charlotte Valset <charlottevalset@gmail.com>
2024-05-08 15:35:14 +0200

    small fixes on contact form

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,14 @@
 import { Header } from "./Header"
 import { Footer } from "./Footer"
 import { ProductCard } from "../Card"
+import { ContactForm } from "../Pages/contact"
 
 
 export const Layout = () => {
   return (
     <div>
       <Header />
-      <ProductCard />
+      <ContactForm />
       <Footer />
     </div>
   )
--- src/components/Pages/contact/index.jsx
+++ src/components/Pages/contact/index.jsx
@@ -1,59 +1,59 @@
 export const ContactForm = () => {
   return (
     <>
-      <h1 className="text-4xl text-center mt-24 mb-5">Contact us</h1>
-      <p className="text-2xl text-gray-400 text-center">
+      <h1 className="text-4xl text-center text-purple-pink font-semibold mt-24 mb-5">Contact us</h1>
+      <p className="text-2xl text-gray-600 text-center font-light my-8 w-5/6 max-w-sm mx-auto">
         Please let us know if theres is anything we can help you with!
       </p>
-      <div className="m-auto max-w-2xl mt-8 py-10 rounded">
+      <div className="m-auto max-w-2xl py-6">
         <form className="w-5/6 max-w-sm mx-auto">
           <div className="mb-5">
-            <label for="fullName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="fullName" className="block mb-2 text-lg text-gray-900 dark:text-white">
               Full name
             </label>
             <input
               type="text"
               id="fullName"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               required
             />
           </div>
           <div className="mb-5">
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="email" className="block mb-2 text-lg text-gray-900 dark:text-white">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               required
             />
           </div>
           <div className="mb-5">
-            <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="subject" className="block mb-2 text-lg text-gray-900 dark:text-white">
               Subject
             </label>
             <input
               type="text"
               id="subject"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              className="bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               required
             />
           </div>
           <div>
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label for="message" class="block mb-2 text-lg text-gray-900 dark:text-white">
                 Your message
               </label>
               <textarea
                 id="message"
                 rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 border border-gray-400 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               ></textarea>
           </div>
           <div className="flex justify-center mt-8">
           <button
             type="submit"
-            className="text-white bg-gray-950 hover:bg-pink-500 focus:ring-4 focus:outline-none font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-gray-950 hover:bg-pink-500 font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Send message
           </button>

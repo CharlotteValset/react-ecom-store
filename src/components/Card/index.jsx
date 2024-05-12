c463dd3f631c7efe7a2b3489e64718947c0ee4ab
Charlotte Valset <charlottevalset@gmail.com>
2024-05-12 18:08:44 +0200

    added more width to card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,85 @@
 import Dress from "../../assets/images/dress.jpg";
-/* import Watch from "../../assets/images/watch.jpg"
- */ export const ProductCard = () => {
+import Watch from "../../assets/images/watch.jpg"
+
+export const ProductCard = () => {
   return (
     <>
       <div className="mt-20 flex flex-wrap gap-8 max-w-5xl mx-auto">
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
+          <div className="flex flex-col">
+            <div className="">
+              <img className="w-full h-64 object-cover" src={Watch} alt="product image" />
+            </div>
+            <div className="py-3 px-2">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Watch</h5>
+              <div className="flex items-center mt-2.5 mb-5">
+                <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                  <svg
+                    className="w-3 h-3 text-purple-pink"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-3 h-3 text-purple-pink"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-3 h-3 text-purple-pink"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-3 h-3 text-purple-pink"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-3 h-3 text-gray-200 dark:text-gray-600"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                </div>
+                <span className="bg-light-pink text-purple-pink text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                  4.0
+                </span>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>
+                  <span className="text-base font-bold text-gray-900 pe-4 dark:text-white">$599</span>
+                  <span className="text-base line-through text-gray-500 font-extralight dark:text-white">$498</span>
+                </div>
+                <a
+                  href="#"
+                  className=" text-white bg-gray-950 hover:bg-pink-500 font-medium text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  View product
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
           <div className="flex flex-col">
             <div className="">
               <img className="w-full h-64 object-cover" src={Dress} alt="product image" />
@@ -78,7 +153,7 @@ import Dress from "../../assets/images/dress.jpg";
             </div>
           </div>
         </div>
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
           <div className="flex flex-col">
             <div className="">
               <img className="w-full h-64 object-cover" src={Dress} alt="product image" />
@@ -152,7 +227,7 @@ import Dress from "../../assets/images/dress.jpg";
             </div>
           </div>
         </div>
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
           <div className="flex flex-col">
             <div className="">
               <img className="w-full h-64 object-cover" src={Dress} alt="product image" />
@@ -226,7 +301,7 @@ import Dress from "../../assets/images/dress.jpg";
             </div>
           </div>
         </div>
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
           <div className="flex flex-col">
             <div className="">
               <img className="w-full h-64 object-cover" src={Dress} alt="product image" />
@@ -300,81 +375,7 @@ import Dress from "../../assets/images/dress.jpg";
             </div>
           </div>
         </div>
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
-          <div className="flex flex-col">
-            <div className="">
-              <img className="w-full h-64 object-cover" src={Dress} alt="product image" />
-            </div>
-            <div className="py-3 px-2">
-              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Blue dress</h5>
-              <div className="flex items-center mt-2.5 mb-5">
-                <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                  <svg
-                    className="w-3 h-3 text-purple-pink"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                  <svg
-                    className="w-3 h-3 text-purple-pink"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                  <svg
-                    className="w-3 h-3 text-purple-pink"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                  <svg
-                    className="w-3 h-3 text-purple-pink"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                  <svg
-                    className="w-3 h-3 text-gray-200 dark:text-gray-600"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                </div>
-                <span className="bg-light-pink text-purple-pink text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                  4.0
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <div>
-                  <span className="text-base font-bold text-gray-900 pe-4 dark:text-white">$599</span>
-                  <span className="text-base line-through text-gray-500 font-extralight dark:text-white">$498</span>
-                </div>
-                <a
-                  href="#"
-                  className=" text-white bg-gray-950 hover:bg-pink-500 font-medium text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  View product
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-10 mx-auto w-64 shadow-xl bg-white">
+        <div className="mt-10 mx-auto w-80 shadow-xl bg-white">
           <div className="flex flex-col">
             <div className="">
               <img className="w-full h-64 object-cover" src={Dress} alt="product image" />

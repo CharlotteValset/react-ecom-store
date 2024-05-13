35839c8869e1f915c66aa634868e11fdb42aa7df
Charlotte Valset <charlottevalset@gmail.com>
2024-05-13 11:27:52 +0200

    adds cta on checkout success page

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,12 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { ProductCard } from "../../components/Card"
+import { CheckoutSuccess } from "../../components/Pages/CheckoutSuccessPage";
 
 export const Layout = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <ProductCard />
+      <CheckoutSuccess />
       <Footer />
     </div>
   );

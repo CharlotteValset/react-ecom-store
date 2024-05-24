76f589c6bf45fb1e8a78b3e9c1d94147d929322d
Charlotte Valset <charlottevalset@gmail.com>
2024-05-24 13:11:48 +0200

    add reviewCard component

---
+++ src/components/ReviewCard/index.jsx
@@ -0,0 +1,26 @@
+export const ReviewCard = ({ data }) => {
+  return (
+    <section className="my-5">
+      <div className="flex flex-row items-center gap-4">
+        <h3 className="font-bold">{data.username}</h3>
+        <div className="flex items-center">
+          <span className="flex flex-row items-center bg-light-pink text-purple-pink text-xs font-semibold px-2 py-0.5 rounded">
+            <svg
+              className="w-3 h-3 text-purple-pink me-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            {data.rating}
+          </span>
+        </div>
+      </div>
+      <div>
+        <p>{data.description}</p>
+      </div>
+    </section>
+  );
+};

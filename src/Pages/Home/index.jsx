babb14aa4a5b42edeaf3dc58c741eaa055ccf823
Charlotte Valset <charlottevalset@gmail.com>
2024-05-24 10:47:45 +0200

    adds and fetches product cards to homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,25 @@
-import { Hero } from "../../components/Hero";
-import { SearchBar } from "../../components/SearchBar";
-import { ProductCard } from "../../components/ProductCard";
+import { Hero, SearchBar, ProductCard, Loader } from "../../components";
+import { useFetch } from "../../hooks/useFetch";
+import { apiUrl } from "../../common/constants";
 
 export const Home = () => {
+  const { data, isLoading, hasError } = useFetch(apiUrl);
+
+  let content;
+
+  if (isLoading) {
+    content = <Loader />;
+  } else if (hasError) {
+    content = <div>Error when trying to load the page</div>;
+  } else {
+    content = data.map((product) => <ProductCard data={product} key={product.id} />);
+  }
+
   return (
-    <>
+    <main>
       <Hero />
       <SearchBar />
-      <ProductCard />
-    </>
+      <div className="mt-10 flex flex-wrap gap-8 max-w-7xl mx-auto px-9">{content}</div>
+    </main>
   );
 };

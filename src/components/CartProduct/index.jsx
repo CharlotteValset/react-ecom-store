b728f879e01df9fc90d04bb5e46285bd5604166e
Charlotte Valset <charlottevalset@gmail.com>
2024-05-29 09:50:19 +0200

    adds all shopping cart functions

--- src/components/CartProduct/index.jsx
+++ src/components/CartProduct/index.jsx
@@ -1,17 +1,26 @@
-import Watch from "../../assets/images/watch.jpg";
 import { QuantityCounter } from "../QuantityCounter";
+import { useStore } from "../../hooks/useStore";
 
-export const CartProduct = () => {
+export const CartProduct = ({ product }) => {
+  const removeFromCart = useStore((state) => state.removeFromCart);
+
+  if (!product) {
+    return null;
+  }
+
   return (
     <>
-      <div className="my-4 mx-auto w-72 shadow-xl bg-white">
+      <div className="my-4 mx-auto w-72 xs:w-96 shadow-xl bg-white">
         <div className="flex items-center justify-start w-full">
-          <img className="w-24 h-24 object-cover" src={Watch} alt="product image" />
+          <img className="w-28 h-28 object-cover" src={product.image.url} alt={product.image.alt} />
           <div className="flex flex-col basis-3/4">
-            <h5 className="text-xl font-medium tracking-tight py-1 ms-8 text-gray-900 dark:text-white">Watch</h5>
+            <h5 className="text-lg font-medium tracking-tight py-1 ms-8 text-gray-900 dark:text-white">
+              {product.title}
+            </h5>
+            <p className="text-base font-medium tracking-tight py-1 ms-8 text-gray-900">${product.discountedPrice}</p>
             <div className="flex flex-row gap-8 items-center">
-              <QuantityCounter />
-              <a className="py-1 delete-icon">
+              <QuantityCounter quantity={product.quantity} productId={product.id} />
+              <a className="py-1 delete-icon" onClick={() => removeFromCart(product.id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path
                     stroke="currentColor"

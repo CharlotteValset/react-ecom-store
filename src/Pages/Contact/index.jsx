0da582d2322fb710ff5ea3cf7c2f91d8690ef559
Charlotte Valset <charlottevalset@gmail.com>
2024-05-29 12:03:30 +0200

    adds validation to contact form with yup

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,4 +1,30 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    fullName: yup.string().min(3, "Full name should be at least 3 characters.").required(),
+    email: yup.string().email("Please enter a valid email address").required("Please enter a valid email address"),
+    subject: yup.string().min(3, "Subject should be at least 3 characters.").required(),
+    messageBody: yup.string().min(3, "Message should be at least 3 characters.").required(),
+  })
+  .required();
+
 export const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    alert("Your message was sent!");
+  };
+
   return (
     <>
       <h1 className="text-4xl text-center text-purple-pink font-semibold mt-24 mb-5">Contact us</h1>
@@ -6,49 +32,61 @@ export const Contact = () => {
         Please let us know if theres is anything we can help you with!
       </p>
       <div className="py-6 flex justify-center">
-        <form className="w-5/6 max-w-xs">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-5/6 max-w-xs">
           <div className="mb-5">
             <label htmlFor="fullName" className="block mb-1 text-lg text-gray-900 dark:text-white">
               Full name
             </label>
             <input
+              {...register("fullName", {
+                required: true,
+                minLength: 3,
+              })}
               type="text"
-              id="fullName"
               className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              required
             />
+            <p className="text-sm text-purple-pink">{errors.fullName?.message}</p>
           </div>
           <div className="mb-5">
             <label htmlFor="email" className="block mb-1 text-lg text-gray-900 dark:text-white">
               Email
             </label>
             <input
+              {...register("email", {
+                required: true,
+              })}
               type="email"
-              id="email"
               className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              required
             />
+            <p className="text-sm text-purple-pink">{errors.email?.message}</p>
           </div>
           <div className="mb-5">
             <label htmlFor="subject" className="block mb-1 text-lg text-gray-900 dark:text-white">
               Subject
             </label>
             <input
+              {...register("subject", {
+                required: true,
+                minLength: 3,
+              })}
               type="text"
-              id="subject"
               className="bg-gray-50 border border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              required
             />
+            <p className="text-sm text-purple-pink">{errors.subject?.message}</p>
           </div>
           <div className="">
             <label htmlFor="message" className="block mb-1 text-lg text-gray-900 dark:text-white">
               Your message
             </label>
             <textarea
-              id="message"
+              {...register("messageBody", {
+                required: true,
+                minLength: 3,
+              })}
               rows="4"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 border border-gray-400 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             ></textarea>
+            <p className="text-sm text-purple-pink">{errors.messageBody?.message}</p>
           </div>
           <div className="flex justify-center mt-8">
             <button
